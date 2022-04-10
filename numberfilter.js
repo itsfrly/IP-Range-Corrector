be4952c861478031400352fdcf2e3333953c7375
Andrew Farley <andrewfarley@MacBook-Pro.local>
2022-04-10 07:36:11 -0700

    Remove commented code.

--- numberfilter.js
+++ numberfilter.js
@@ -20,29 +20,6 @@ ipArr.forEach(x => {
     else{
         newList.push(x)
     }
-    
-   /* let octet=1
-    let lastOctet2 = []
-    let lastOctet = []
-    let currentObject = x //current element of array
-    for(let i=0; i < currentObject.length; i++) { //iterate through numbers in ip
-        if(currentObject.length > 12){ //All the magic happens in here
-            if(currentObject[i] === '.'){
-                    octet += 1
-                    if(octet === 4 ){ //once in 4th octet
-                        lastOctet.push(currentObject[i])
-                    }
-                    if(octet === 8){
-                        lastOctet2.push(currentObject[i])
-                    }
-                }
-            }
-        }
-    console.log(lastOctet)
-    console.log(lastOctet2)    
-    for(let j=lastOctet; j < lastOctet2; j++){
-    
-    } */
 })
 
 console.dir(newList, {'maxArrayLength':null})
